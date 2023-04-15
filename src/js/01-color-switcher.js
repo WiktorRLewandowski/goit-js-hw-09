d9780237fb6e37b21e4794e37e5fc1ff2d510ae9
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-16 00:04:55 +0200

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let interval = null;
 
+startBtn.addEventListener('click', () => {
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+});
