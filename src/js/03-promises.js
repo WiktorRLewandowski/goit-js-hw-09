6b24d5f3b6c1a919a7acd0b6beffc58ebd142870
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-04-23 20:55:34 +0200

    03-promises done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault(event);
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let formDelay = +delay.value;
+  let formStep = +step.value;
+  let formAmount = +amount.value;
+
+  for (let i = 1; i <= formAmount; i++) {
+    createPromise(i, formDelay + i * formStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
